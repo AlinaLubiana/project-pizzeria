0724a26ebc18c729eefb537d3c15bc5ce499d1e7
Alina Lubiana <alina.furman28@gmail.com>
2022-12-09 12:48:23 +0100

    add class Product and app.initMenu
    Створення класу
    Створення першого екземпляра

--- dist/js/script.js
+++ dist/js/script.js
@@ -51,8 +51,19 @@
   const templates = {
     menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
   };
+  class Product{
+    constructor(){
+      const thisProdact = this;
+
+      console.log('new Prodact:', thisProdact);
+    }
+  }
 
   const app = {
+    initMenu: function(){
+      const testProduct = new Product();
+      console.log('testProduct', testProduct);
+    },
     init: function(){
       const thisApp = this;
       console.log('*** App starting ***');
@@ -60,8 +71,10 @@
       console.log('classNames:', classNames);
       console.log('settings:', settings);
       console.log('templates:', templates);
+
+      thisApp.initMenu();
     },
   };
-
+  
   app.init();
 }
