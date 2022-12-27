b477c4b374c97d4e512c005cf01dae3e25159949
Alina  Lubiana <alina.furman28@gmail.com>
2022-12-27 12:56:02 +0200

    8.6 Запуск події

--- dist/js/script.js
+++ dist/js/script.js
@@ -431,6 +431,7 @@
       thisCartProduct.priceSigle = menuProduct.priceSigle;
       thisCartProduct.getElements(element);
       thisCartProduct.initAmountWidget();
+      thisCartProduct.initActions();
       // console.log('thisCartProduct', thisCartProduct);
 
     }
@@ -460,6 +461,32 @@
       thisCartProduct.dom.price.innerHTML = thisCartProduct.price;
       // console.log('thisCartProduct.price', thisCartProduct.price);
       }); 
+    }
+
+    remove(){
+      const thisCartProduct = this;
+
+      const event = new CustomEvent('remove', {
+        bubbles: true,
+        detail: {
+          cartProduct: thisCartProduct,
+        },
+      });
+
+      thisCartProduct.dom.wrapper.dispatchEvent(event);
+      console.log('thisCartProduct.dom.wrappert', thisCartProduct.dom.wrapper);
+    }
+
+    initActions(){
+      const thisCartProduct = this;
+
+      thisCartProduct.dom.edit.addEventListener('click', function(event) {
+        event.preventDefault();
+      });
+      thisCartProduct.dom.remove.addEventListener('click', function(event) {
+        event.preventDefault();
+        thisCartProduct.remove();
+      });
       
     }
 
