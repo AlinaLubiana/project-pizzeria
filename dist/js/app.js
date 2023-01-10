2ca2920f104117f66280973996315bc2419c657f
Alina Lubiana <alina.furman28@gmail.com>
2023-01-10 16:01:02 +0100

    add 9.4 function from video

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,18 +1,73 @@
-import {select ,classNames, settings, templates} from './settings.js';
-import utils from './utils.js';
+import {select, classNames, settings} from './settings.js';
+// import utils from './utils.js';
 // import CartProduct from './components/CartProduct.js';
 // import AmountWidget from './components/AmountWidget.js';
 import Cart from './components/Cart.js';
 import Product from './components/Product.js';
 
 
-
-
-
-
-
-
 const app = {
+  initPages: function(){
+    const thisApp = this;
+
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+
+    const idFromHash = window.location.hash.replace('#/', '');
+    // console.log('idFromHash', idFromHash);
+
+    let pageMatchingHash = thisApp.pages[0].id;
+
+    for(let page of thisApp.pages){
+      if(page.id == idFromHash){
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+
+    thisApp.activatePage(idFromHash);
+
+    for(let link of thisApp.navLinks){
+      link.addEventListener('click', function(event){
+        const clickedElement = this;
+        event.preventDefault();
+
+        /* get page id from href attribute */
+        const id = clickedElement.getAttribute('href').replace('#', '');
+
+        /* run thisApp.activatePage with that id */
+        thisApp.activatePage(id);
+
+        /* change URL hash*/
+        window.location.hash = '#/' + id;
+      });
+    }
+  },
+  activatePage: function(pageId){
+    const thisApp = this;
+
+    /* add class "active" to matching pages, remove from non-matching*/
+
+    for(let page of thisApp.pages){
+      // if(page.id == pageId){
+      //   page.classList.add(classNames.pages.active);
+      // } else {
+      //   page.classList.remove(classNames.pages.active);
+      // }
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+    }
+
+    /* add class "active" to matching links, remove from non-matching*/
+    for(let link of thisApp.navLinks){
+      link.classList.toggle(
+        classNames.nav.active, 
+        link.getAttribute('href') == '#' + pageId
+      );
+    }
+
+
+  },
+
   initMenu: function(){
     const thisApp = this;
     // console.log('thisApp.data', thisApp.data);
@@ -25,11 +80,13 @@ const app = {
 
   init: function(){
     const thisApp = this;
-    console.log('*** App starting ***');
-    console.log('thisApp:', thisApp);
-    console.log('classNames:', classNames);
-    console.log('settings:', settings);
-    console.log('templates:', templates);
+    // console.log('*** App starting ***');
+    // console.log('thisApp:', thisApp);
+    // console.log('classNames:', classNames);
+    // console.log('settings:', settings);
+    // console.log('templates:', templates);
+
+    thisApp.initPages();
 
     thisApp.initData();
     // thisApp.initMenu();
